b1a83f9a3eec7f77c53f0d0d5bf14407589b41a0
MPoyi Tshibuyi <frankmpoyi63@gmail.com>
2023-06-28 01:26:15 +0200

    add debug

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,6 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+DEBUG = bool(config("DEBUG"))
+
 ALLOWED_HOSTS = ['*' , 'localhost', '127.0.0.1', 'driver-logsheet-441c01a3345a.herokuapp.com' ]
 
 AUTH_USER_MODEL = 'account.CustomUser'

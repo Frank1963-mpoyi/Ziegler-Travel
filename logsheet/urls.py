8157c492faf858051bc84b4fc03785a192cbe895
MPoyi Tshibuyi <frankmpoyi63@gmail.com>
2023-06-27 12:32:05 +0200

    url

--- logsheet/urls.py
+++ logsheet/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import logsheet_list_view #, create_logsheet
 
 urlpatterns = [
-    path('list-logsheet', logsheet_list_view, name='list-logsheet'),
+    path('', logsheet_list_view, name='list-logsheet'),
 
 ] 
